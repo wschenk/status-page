1058de87b11c1a07dec16447bee2940922a33872
Will Schenk <wschenk@gmail.com>
2024-08-26 10:56:31 -0400

    Testing on prod!

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
+const base = process.env.BASE_DIR || "/";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/status-page/",
+  base,
 });
